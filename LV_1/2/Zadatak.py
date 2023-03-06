eaa54a0b0e611824487293ee1905c48ffc965497
Schime <117494827+Schime@users.noreply.github.com>
2023-03-06 13:02:14 +0100

    Add files via upload

--- LV_1/2/Zadatak.py
+++ LV_1/2/Zadatak.py
@@ -1,22 +1,22 @@
 try:
     input = float(input("Upisite broj u rasponu [0.0 1.0]: "))
 
-    if(input < 0.0 or input > 1.0):
+    if input < 0.0 or input > 1.0:
         print("Greška: broj nije unutar intervala!")
 
     elif(input < 0.6):
         print("F")
 
-    elif(input >= 0.6 & input < 0.7):
+    elif(0.6 <= input < 0.7):
         print("D")
 
-    elif(input >= 0.7 & input < 0.8):
+    elif(0.7 <= input < 0.8):
         print("C")
 
-    elif(input >= 0.8 & input < 0.9):
+    elif(0.8 <= input < 0.9):
         print("B")
 
-    elif(input >= 0.9 & input < 1):
+    elif(input >= 0.9):
         print("A")
 
 except ValueError:
